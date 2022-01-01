79a978359c2e8781be616f4471ccc3b3afffad89
Alex Kelley <alexkelley@Alexs-MBP.webpass.net>
2022-01-01 10:34:00 -0800

    Day 2, first commit

--- lib/day01.ex
+++ lib/day01.ex
@@ -2,6 +2,7 @@ defmodule Day01 do
   @moduledoc """
   Part 1 - How many measurements are larger than the previous measurement?
   Part 2 - How many sums are larger than the previous sum?
+  See https://adventofcode.com/2021/day/1
   """
 
  @sample """
@@ -17,6 +18,11 @@ defmodule Day01 do
   263
   """
 
+def puzzle_input do
+   "lib/day01_input.txt"
+   |> File.read!()
+end
+#  
  @doc """
   :input "199 200 ... 
   :output [199, 200, ...]
@@ -36,7 +42,8 @@ defmodule Day01 do
 
   """
   def part1 do
-    @sample
+    #@sample
+    puzzle_input
     |> parse_input
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.count(fn [left, right] -> right > left end)
@@ -50,7 +57,8 @@ defmodule Day01 do
 
   """
   def part2 do
-    @sample
+    #@sample
+    puzzle_input
     |> parse_input
     |> Enum.chunk_every(3, 1, :discard)
     |> Enum.map(&Enum.sum/1)
---
+++ lib/day02.ex
@@ -0,0 +1,18 @@
+defmodule Day02 do
+  @moduledoc """
+  Part 1 - Calculate the horizontal position and depth you would have after following the planned course.
+  What do you get if you multiply your final horizontal position by your final depth?
+
+  Part 2 - 
+  
+  """
+
+  @sample """
+  forward 5
+  down 5
+  forward 8
+  up 3
+  down 8
+  forward 2
+  """
+end
