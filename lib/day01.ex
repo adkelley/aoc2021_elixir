ddc2b54b48d616c5d25b91098320498456b4e4a0
Alex Kelley <alexkelley@Alexs-MBP.webpass.net>
2022-01-01 10:05:31 -0800

    Day 1 completed

---
+++ lib/day01.ex
@@ -0,0 +1,60 @@
+defmodule Day01 do
+  @moduledoc """
+  Part 1 - How many measurements are larger than the previous measurement?
+  Part 2 - How many sums are larger than the previous sum?
+  """
+
+ @sample """
+  199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263
+  """
+
+ @doc """
+  :input "199 200 ... 
+  :output [199, 200, ...]
+ """
+ def parse_input(input) do
+   input
+   |> String.split("\n", trim: true)
+   |> Enum.map(&String.to_integer/1)
+ end
+
+  @doc """
+
+  ## Examples
+
+      iex> Day01.part1
+      7
+
+  """
+  def part1 do
+    @sample
+    |> parse_input
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [left, right] -> right > left end)
+  end
+
+  @doc """
+
+  ## Examples
+      iex> Day01.part2
+      5
+
+  """
+  def part2 do
+    @sample
+    |> parse_input
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [left, right] -> right > left end)
+  end
+end
